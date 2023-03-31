6a97e21c614d157b9e7accef3c6640ca606cd776
aleksmn <al-mn@yandex.ru>
2023-03-31 15:49:26 +0300

    init

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+    // Выводим в консоль путь запроса и метод запроса
+    console.log(req.path, req.method)
+
+    next()
+})
+
+// routes
+app.get('/', (req, res) => {
+    res.json({ msg: 'Добро пожаловать!' })
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port 5000')
+})
