a69efafbe949192d501c51eb9ff9685cde13cd2c
aleksmn <al-mn@yandex.ru>
2023-04-12 22:46:47 +0300

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+
+const cardRoutes = require('./routes/cards')
 
 const app = express()
-const cardRoutes = require('./routes/cards')
 
 // middleware
 app.use(express.json())
@@ -17,9 +19,17 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/cards', cardRoutes)
+ 
+
+// connect DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port ' + process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
-
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port 5000')
-})
