dc114e9056b85a61e9f36f6524995c10125b9a0a
aleksmn <al-mn@yandex.ru>
2023-04-25 20:40:19 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/cards', cardRoutes)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log('connected to db and listening on port ' + process.env.PORT)
+            console.log('connected to db and listening on port http://localhost:' + process.env.PORT)
         })
     })
     .catch((error) => {
