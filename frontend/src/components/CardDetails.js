150d1e4c98f9e8312da19912d999b89bc4b69cbf
aleksmn <al-mn@yandex.ru>
2023-05-04 18:35:39 +0300

    add DELETE card functionality

--- frontend/src/components/CardDetails.js
+++ frontend/src/components/CardDetails.js
@@ -1,9 +1,28 @@
+import { useCardsContext } from '../hooks/useCardsContext';
+
 const CardDetails = ({ card }) => {
+
+    const { dispatch } = useCardsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/cards/' + card._id, {
+            method: 'DELETE'
+        })
+
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_CARD', payload: json})
+        }
+
+    }
 
     return (
         <div className="card-details">
             <h5>{card.title}</h5>
             <p>{card.content}</p>
+            <span onClick={handleClick} style={{"color":"red"}}>Удалить</span>
+
             <hr />
         </div>
     )
